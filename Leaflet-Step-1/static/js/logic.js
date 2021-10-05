0bd7b2cf5dd06dcb4aed548f4998c3f77a045ae7
carole koudjonou <carolekoudjonou@yahoo.fr>
2021-10-04 19:20:57 -0500

    final

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,75 +1,122 @@
-// Create a map object.
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-  });
-  
-  // Add a tile layer.
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
-}).addTo(myMap);
+// store geoJSON
+const earth = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-//// Store our API endpoint as Url.
-var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 
-// perform the Get request
-d3.json (url).then(function (data){
-// Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3> Location ${feature.properties.place} <br>Magnitude:${feature.properties.mag}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
-  
-});
+//get request
+d3.json('https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json').then(function(response){
+    const tectonicplates = L.geoJSON(response,{  
+        style: function(feature){
+            return{
+                color: "#B7DF5F",
+                weight: 3
+            }
+        }
+    })
 
-function Raduis(depth) {
-  return Math.sqrt(depth / 3.1415);
+    d3.json(earth).then((data) => {
+        var earthQuakes = L.geoJSON(data,{
+            onEachFeature: function(feature,layer){
+                layer.bindPopup("<h1>Earthquake: " + feature.properties.place + "</h1> <hr> Time: " + new Date (feature.properties.time) + "<hr> Magnitude: " + feature.properties.mag + "<hr> Significance: " + feature.properties.sig, {maxWidth: 400});
+            },
+            pointToLayer: addCircleMarker
+            });
+        function addCircleMarker (feature, latlng){
+            let options={
+                radius: feature.properties.mag * 3,
+                stroke:false,
+                fillColor: mapColor(feature.properties.sig),
+                fillOpacity: 0.75,
+            }
+            return L.circleMarker( latlng, options );
+        }
+        createMap(earthQuakes, tectonicplates);    
+    })
+})
+
+
+//Create Map Function
+function createMap(earthQuakes, tectonicplates) {
+    const dark= L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/dark-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
+        });
+
+    const light= L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
+        });
+
+    const sattelite= L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/satellite-v9',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
+        });
+
+    const baseMaps = {
+        Light: light,
+        Dark: dark,
+        Sattelite: sattelite
+        };
+
+    const overlayMaps = {
+        "Earthquakes" : earthQuakes,
+         "tectonicplates": tectonicplates
+        };
+
+    let myMap = L.map('map', {
+        center: [0, 0],
+        zoom: 3,
+        layers: [dark, earthQuakes,tectonicplates]
+        });
+
+    
+    L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+        }).addTo(myMap);
+    
+    //Add legend
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function () {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML += "<h5>earthquakes magnitude</h5>";
+    grade = [1, 2, 3, 4, 5];
+    for (var i = 0; i < grade.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + mapColor(grade[i] + 1) + '"></i> ' + 
+    + grade[i] + (grade[i + 1] ? ' - ' + grade[i + 1] + '<br><br>' : ' + ');
+    }
+    return div;
+};
+
+legend.addTo(myMap);
+
 }
 
-//create a marker color based on magnitude
-function markerSize(magnitude){
-  if (magnitude > 10 ) {
-    return '#8B0000'
-  }
-
-  if (magnitude > 9 ) {
-    return '#8B22222'
-  }
-
-  if (magnitude > 8 ) {
-    return '#FF0000'
-  }
-
-  if (magnitude > 7 ) {
-    return '#DAA520'
-  }
-
-  if (magnitude > 6 ) {
-    return '#FFD700'
-  }
-
-  if (magnitude > 5 ) {
-    return '#FFFF00'
-  }
-
-  if (magnitude > 4 ) {
-    return '#3CB371'
-  }
-
-  if (magnitude > 3 ) {
-    return '#98FB98'
-  }
-
-  if (magnitude > 2 ) {
-    return '#B0E0E6'
-  }
-
-  if (magnitude > 1 ) {
-    return '#FFFFFF'
+// Define function for Market Color
+function mapColor(mag) {
+  switch (true) {
+    case mag > 5:
+      return "#ea2c2c";
+    case mag > 4:
+      return "#eaa92c";
+    case mag > 3:
+      return "#d5ea2c";
+    case mag > 2:
+      return "#92ea2c";
+    case mag > 1:
+      return "#2ceabf";
+    default:
+      return "#2c99ea";
   }
 };
